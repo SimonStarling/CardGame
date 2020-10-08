dc3ccbbc8b7e669892a82dacdb328925e2780e61
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-10-08 18:02:16 +0200

    Skapat meny, txtfil med regler och påbörjat själva spelfunktionen. Färdig med kortleksklasserna.

--- cards.py
+++ cards.py
@@ -1,14 +1,17 @@
 '''
 Här ska jag skapa själva kortleken som sedan kommer användas i 'data.py'
 '''
+import random
+
 
 class Playingcards():
-    def __init__(self, suit, value):
+    def __init__(self, value, suit):
         self.value = value
         self.suit = suit
 
     def show(self):
-        print(f"{} av {}".format(self.value, self.suit))
+        print(f"{self.value} {self.suit}")
+
 
 class DeckOfCards():
     def __init__(self):
@@ -16,4 +19,16 @@ class DeckOfCards():
         self.build()
 
     def build(self):
-        for c in ["Hjärter", "Spader", "Ruter", "Klöver"]:
+        for card in ["Hjärter", "Spader", "Ruter", "Klöver"]:
+            for value in range(1,14):
+                self.cards.append(Playingcards(card, value))
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def DeckShuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+    def DrawCard(self):
+        return self.cards.pop()
--- data.py
+++ data.py
@@ -1,3 +1,27 @@
 '''
 Här ska själva logiken ske. Ta emot userinput och ge output. Ska nyttjas av main.py och hämta 'kortleken' från cards.py
 '''
+from pathlib import Path
+from cards import *
+
+
+def ViewRules():
+    p = Path('CardGame', 'gamerules.txt')
+    rules = p.read_text(encoding='utf8')
+    return rules
+
+
+def Game():
+    players = input("Skriv in era namn. Ange ett kommatecken mellan varje namn: ").split()
+    if len(players) <= 0:
+        print("Inga namn angavs... Gör om och gör rätt!")
+    elif len(players) <= 1:
+        print("Inte så kul att spela själv eller? Bjud in lite folk och försök igen!")
+    else:
+        print(f"Spelare {players} Antal spelare {len(players)}\nNu kör vi igång!")
+
+
+
+
+if __name__ == '__main__':
+    Game()
